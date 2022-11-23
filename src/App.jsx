8a8bfbf2cfae49e0cbcf4ae534228140a5254f5e
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-11-23 20:29:50 +0530

    created GlobalStyles, added routes & minor improvements
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Player from "./components/Player/Player";
+import Navbar from "./components/Navbar/TopNav";
+import Home from "./pages/Home/Home";
+
 const App = () => {
-	return <div></div>;
+	return (
+		<Router>
+			<GlobalStyles />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/playlist" />
+				<Route path="/charts" />
+				<Route path="/search" />
+			</Routes>
+			<Navbar />
+			<Player />
+		</Router>
+	);
 };
 
 export default App;
