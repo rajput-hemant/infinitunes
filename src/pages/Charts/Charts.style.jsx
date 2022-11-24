7d41213a5feed38ed4024991c8b27b0c8c7eb7fa
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-11-24 11:19:43 +0530

    created other page component
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ src/pages/Charts/Charts.style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	color: white;
+	display: flex;
+	position: absolute;
+	top: 50%;
+	justify-content: center;
+	height: 100%;
+	width: 100%;
+	font-family: Roboto;
+	font-size: xx-large;
+	font-weight: 700;
+`;
---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./Search.style";
+
+const Search = () => {
+	return <Container>Search Screen</Container>;
+};
+
+export default Search;
