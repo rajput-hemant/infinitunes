80fc608e62834a1627367776d8b55808094fca6e
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-11-24 11:16:06 +0530

    added Theme Switcher hook, added Roboto fonts, added light & dark mode themes, and minor improvements
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import GlobalStyles from "./styles/GlobalStyles";
+import GlobalStyles, { themes } from "./styles/GlobalStyles";
+import useThemeSwitcher from "./styles/ThemeSwitcher";
 import Player from "./components/Player/Player";
 import Navbar from "./components/Navbar/TopNav";
 import Home from "./pages/Home/Home";
+import Playlists from "./pages/Playlists/Playlists";
+import Charts from "./pages/Charts/Charts";
+import Settings from "./pages/Settings/Settings";
+import Search from "./pages/Search/Search";
+import About from "./pages/About/About";
+import { ThemeProvider } from "styled-components";
 
 const App = () => {
+	const [theme, toggleTheme] = useThemeSwitcher();
+	const themeMode = theme === "light" ? themes.light : themes.dark;
+
 	return (
 		<Router>
-			<GlobalStyles />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/playlist" />
-				<Route path="/charts" />
-				<Route path="/search" />
-			</Routes>
-			<Navbar />
-			<Player />
+			<ThemeProvider theme={themeMode}>
+				<GlobalStyles />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/playlist" element={<Playlists />} />
+					<Route path="/charts" element={<Charts />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+				<Navbar theme={theme} toggleTheme={toggleTheme} />
+				<Player />
+			</ThemeProvider>
 		</Router>
 	);
 };
