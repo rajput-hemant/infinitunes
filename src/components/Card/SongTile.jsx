8a67e47e24682fcbd0f6bd0dc8e94495203986f9
Khushal-ag <starkhush5@gmail.com>
2022-11-27 22:36:17 +0530

    minor style fixes

--- src/components/Card/SongTile.jsx
+++ src/components/Card/SongTile.jsx
@@ -1,15 +1,22 @@
 import { SongIcon, SongInfo, TileContainer } from "./SongTile.style";
+import { FaPlay } from "react-icons/fa";
+
 
 const Tile = (props) => {
 	return (
-		<TileContainer>
-			<SongIcon src={props.image} />
-			<SongInfo>
-				<h4>{props.name}</h4>
-				<h5>{props.artists}</h5>
-			</SongInfo>
-		</TileContainer>
-	);
+    <TileContainer>
+      <div>
+        <SongIcon src={props.image} />
+        <button onClick={console.log(props.name)}>
+          <FaPlay size={25} color="#74f2ce" />
+        </button>
+      </div>
+      <SongInfo>
+        <h4>{props.name}</h4>
+        <h5>{props.artists}</h5>
+      </SongInfo>
+    </TileContainer>
+  );
 };
 
 export default Tile;
