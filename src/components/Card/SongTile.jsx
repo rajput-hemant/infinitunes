093df5af99427999a358894f1ddf64339451e684
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-11-28 14:05:14 +0530

    added framer motion package dependicies,
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/components/Card/SongTile.jsx
+++ src/components/Card/SongTile.jsx
@@ -1,20 +1,24 @@
-import { SongIcon, SongInfo, TileContainer } from "./SongTile.style";
 import { FaPlay } from "react-icons/fa";
+
+import { SongIcon, SongInfo, TileContainer } from "./SongTile.style";
+import { Motion } from "../../styles/Motion";
 
 const Tile = (props) => {
 	return (
-		<TileContainer onClick={() => console.log(props.name)}>
-			<div>
-				<SongIcon src={props.image} />
-				<button>
-					<FaPlay size={25} color="#74f2ce" />
-				</button>
-			</div>
-			<SongInfo>
-				<h4>{props.name}</h4>
-				<h5>{props.artists}</h5>
-			</SongInfo>
-		</TileContainer>
+		<Motion>
+			<TileContainer onClick={() => console.log(props.name)}>
+				<div>
+					<SongIcon src={props.image} />
+					<button>
+						<FaPlay size={25} color="#74f2ce" />
+					</button>
+				</div>
+				<SongInfo>
+					<h4>{props.name}</h4>
+					<h5>{props.artists}</h5>
+				</SongInfo>
+			</TileContainer>
+		</Motion>
 	);
 };
 
--- src/components/Grid/GridItem.jsx
+++ src/components/Grid/GridItem.jsx
@@ -1,17 +1,23 @@
 import { FaPlay } from "react-icons/fa";
+
 import { GridItemCard } from "./GridItem.style";
+import { Motion } from "../../styles/Motion";
 
 const GridItem = ({ src, alt, title, subtitle, onNavigate }) => {
 	return (
 		<GridItemCard>
-			<div>
+			<Motion
+				initial={{ opacity: 0, scale: 0.7 }}
+				whileInView={{ opacity: 1, scale: 1 }}
+				transition={{ duration: 0.5, type: "tween" }}
+			>
 				<img src={src} alt={alt} />
 				<button onClick={() => onNavigate()}>
 					<FaPlay size={50} color="#74f2ce" />
 				</button>
 				<h4>{title}</h4>
 				<h5>{subtitle}</h5>
-			</div>
+			</Motion>
 		</GridItemCard>
 	);
 };
