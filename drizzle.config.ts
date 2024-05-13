4058d29d1b8be35c4bca304390ac9aac4d8d7c69
rajput-hemant <rajput.hemant2001@gmail.com>
2024-05-14 00:37:49 +0530

    feat: migrate drizzle-kit to v0.21.0
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,22 @@
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+
 import type { Config } from "drizzle-kit";
 
 import { siteConfig } from "@/config/site";
-import { env } from "@/lib/env";
+
+loadEnvConfig(cwd());
+
+if (!process.env.DATABASE_URL) {
+  console.error("'DATABASE_URL' is not set in the environment variables");
+  process.exit(1);
+}
 
 export default {
   schema: "./src/lib/db/schema.ts",
   out: "./src/lib/db/migrations",
-  driver: "pg",
-  dbCredentials: { connectionString: env.DATABASE_URL },
+  dialect: "postgresql",
+  verbose: true,
+  dbCredentials: { url: process.env.DATABASE_URL },
   tablesFilter: [`${siteConfig.name.toLowerCase().replace(/\s/g, "_")}_*`],
 } satisfies Config;
